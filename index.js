a29ac0dd9f53804961f47f40bd2598ae5505bb27
Rob Hitt <rob.h@taboola.com>
2021-03-21 23:35:34 -0400

    add test slack message sending to node-sdk channel

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello, World!');
+const dotenv = require('dotenv').config();
+const { WebClient } = require('@slack/web-api');
+const web = new WebClient(process.env.SLACK_TOKEN);
+
+const currentTime = new Date().toTimeString();
+
+(async () => {
+  try {
+    await web.chat.postMessage({
+      channel: 'node-sdk',
+      text: `The current time is ${currentTime}`
+    })
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log('Message posted!');
+})();
